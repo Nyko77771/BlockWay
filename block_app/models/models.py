bcc30c10b86cd2a7e8705519c433a7ea791d68bf
Nyko77771 <nykytamcdonald@gmail.com>
2026-05-03 02:10:11 +0100

    DB Schema

---
+++ block_app/models/models.py
@@ -0,0 +1,88 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, Table, DateTime, func, Enum, CheckConstraint
+from sqlalchemy.orm import relationship, backref, Mapped, mapped_column
+from sqlalchemy.ext.declarative import declarative_base
+import enum
+
+Base = declarative_base()
+
+# Custom List of Values for Severity Attributes
+class ScoreEnum(enum.Enum):
+    CRITICAL = "critical"
+    HIGH = "high"
+    LOW = "low"
+    INFO = "info"
+
+# Custom List of Values for Status Attributes
+class StatusEnum(enum.Enum):
+    OPEN = "open"
+    CLOSED = "closed"
+
+# Association Table
+issue_events = Table(
+    "issue_events",
+    Base.metadata,
+    Column("issue_id", Integer, ForeignKey("issues.issue_id"), primary_key=True),
+    Column("event_id", Integer, ForeignKey("events.event_id"), primary_key=True),
+)
+
+# User Table
+class User(Base):
+    __tablename__ = "users"
+    user_id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String, unique=True)
+    password = Column(String(255), nullable=False)
+    role_type = Column(String)
+    date_created = Column(DateTime(timezone=True) , server_default=func.now())
+    sessions = relationship("UserSession", back_populates="user")
+    events = relationship("Event", back_populates="user")
+
+# Event Table
+class Event(Base):
+    __tablename__ = "events"
+    event_id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey("users.user_id"))
+    event_type  = Column(String, nullable=False)
+    description = Column(String, nullable=False)
+    severity = Column(String, nullable=False, default='info')
+
+    date_created = Column(DateTime(timezone=True) , server_default=func.now())
+    issues = relationship("Issue", secondary=issue_events, back_populates="events")
+    user = relationship("User", back_populates="events")
+
+    __table_args__ = (
+        CheckConstraint(
+           "severity IN ('critical', 'high', 'low', 'info')"
+        ),
+    )
+
+# Table for encountered issues
+class Issue(Base):
+    __tablename__ = "issues"
+    issue_id = Column(Integer, primary_key=True, autoincrement=True)
+    type = Column(String, nullable=False)
+    description = Column(String, nullable=False)
+    severity = Column(String, nullable=False)
+    status = Column(String, nullable=False)
+
+    date_created = Column(DateTime(timezone=True) , server_default=func.now())
+    date_closed = Column(DateTime(timezone=True) , nullable=True)
+
+    events = relationship("Event", secondary=issue_events, back_populates="issues")
+
+    __table_args__ = (
+        CheckConstraint(
+           "severity IN ('critical', 'high', 'low', 'info')"
+        ),
+        CheckConstraint(
+           "status IN ('open', 'closed')"
+        )
+    )
+
+# Session Table
+class UserSession(Base):
+    __tablename__ = "user_sessions"
+    session_id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey("users.user_id"))
+    token  = Column(String)
+    date_created = Column(DateTime(timezone=True))
+    user = relationship("User", back_populates="sessions")
