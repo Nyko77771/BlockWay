2e5d70ba36611dbfd4daf5f91a25754ef8d3c23e
Nyko77771 <nykytamcdonald@gmail.com>
2026-06-27 22:08:49 +0100

    Pihole Service Update

--- tests/pihole_service_test.py
+++ tests/pihole_service_test.py
@@ -4,12 +4,21 @@ import os
 
 # Load .env variables into os environment
 load_dotenv()
+pi_ip = '192.168.9.108'
 
-PIHOLE_AUTH_URL = 'http://localhost:8080/api/auth/'
-PIHOLE_QUERY_URL = 'http://localhost:8080/api/queries'
+PIHOLE_AUTH_URL = f'http://{pi_ip}:8080/api/auth/'
+PIHOLE_QUERY_URL = f'http://{pi_ip}:8080/api/queries'
+PIHOLE_STATS_URL = f'http://{pi_ip}:8080/api/stats/recent_blocked'
+
 password = os.getenv("TEST_API")
 
 def test_can_get_endpoint():
+
+    print('#################################################')
+    print('Sending Request')
+    print(f'To AUTH URL: {PIHOLE_AUTH_URL}')
+    print(f'Password: {password}')
+
     response = requests.post(
     PIHOLE_AUTH_URL,
     json={"password": password},
@@ -29,9 +38,15 @@ def test_can_get_endpoint():
     print(f'CSRF obtained: {csrf}')
 
     return sid, csrf
+    
 
 
 def test_receiving_queries():
+
+    print('#################################################')
+    print('Sending Request')
+    print(f'To: {PIHOLE_QUERY_URL}')
+
 
     sid, csrf = test_can_get_endpoint()
 
@@ -46,7 +61,31 @@ def test_receiving_queries():
 )
 
     assert response.status_code == 200
+    
+
+
+def test_getting_blocked_queries():
+    print('#################################################')
+    print('Sending Request to Recent Blocked')
+    print(f'To: {PIHOLE_STATS_URL}')
+
+
+    sid, csrf = test_can_get_endpoint()
+
+    print(f'SID in receiving queries test {sid}')
+    response = requests.get(
+        PIHOLE_STATS_URL,
+        headers={
+            "X-FTL-SID": sid,
+            "X-FTL-CSRF": csrf
+            },
+        timeout = 5
+)
+
+    assert response.status_code == 200
 
     response_data = response.json()
 
-    print(response_data)
+    blocked = response_data['blocked']
+
+    print(blocked)
